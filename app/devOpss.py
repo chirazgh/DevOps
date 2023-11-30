b6f855c69dfcbb141f58f0d1cd0be59dc5f0df39
chirazgh <gharsallahchiraz97@gmail.com>
2023-11-30 22:31:09 +0100

    first commit

---
+++ app/devOpss.py
@@ -0,0 +1,13 @@
+# Programme de vérification du nom d'utilisateur
+
+# Liste de noms d'utilisateurs
+noms_utilisateurs = ["Chiraz", "Wafa", "Dorra", "Ali", "Ahmed"]
+
+# Demander à l'utilisateur d'entrer son nom
+nom_utilisateur = input("Entrez votre nom : ")
+
+# Vérifier si le nom d'utilisateur est dans la liste
+if nom_utilisateur in noms_utilisateurs:
+    print(f"Bienvenue, {nom_utilisateur} ! Vous êtes un utilisateur enregistré.")
+else:
+    print(f"Désolé, {nom_utilisateur}. Vous n'êtes pas dans la liste des utilisateurs enregistrés.")
