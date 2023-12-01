1af951727063fa99ccb1b6b5c473e8cc8766fc47
chirazgh <gharsallahchiraz97@gmail.com>
2023-12-01 23:37:58 +0100

    Changes

---
+++ app/test.py
@@ -0,0 +1,32 @@
+# test.py
+import unittest
+from username_checker import check_username
+
+class TestUsernameChecker(unittest.TestCase):
+    """
+    Tests pour le programme de vérification du nom d'utilisateur
+    """
+
+    def test_existing_user(self):
+        """
+        Vérification d'un utilisateur existant dans la liste.
+        """
+        result = check_username("Chiraz", ["Chiraz", "Wafa", "Dorra", "Ali", "Ahmed"])
+        self.assertTrue(result)  # Vérifiez que la fonction retourne True pour un utilisateur existant
+
+    def test_non_existing_user(self):
+        """
+        Vérification d'un utilisateur non existant dans la liste.
+        """
+        result = check_username("John", ["Chiraz", "Wafa", "Dorra", "Ali", "Ahmed"])
+        self.assertFalse(result)  # Vérifiez que la fonction retourne False pour un utilisateur non existant
+
+    def test_empty_username(self):
+        """
+        Vérification d'un nom d'utilisateur vide.
+        """
+        result = check_username("", ["Chiraz", "Wafa", "Dorra", "Ali", "Ahmed"])
+        self.assertFalse(result)  # Vérifiez que la fonction retourne False pour un nom d'utilisateur vide
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ app/username_checker.py
@@ -0,0 +1,20 @@
+# username_checker.py
+
+def check_username(username, existing_usernames):
+    """
+    Vérifie si le nom d'utilisateur est dans la liste des utilisateurs existants.
+    """
+    return username in existing_usernames
+
+if __name__ == "__main__":
+    # Liste de noms d'utilisateurs
+    noms_utilisateurs = ["Chiraz", "Wafa", "Dorra", "Ali", "Ahmed"]
+
+    # Demander à l'utilisateur d'entrer son nom
+    nom_utilisateur = input("Entrez votre nom : ")
+
+    # Utiliser la fonction check_username pour vérifier si le nom d'utilisateur est dans la liste
+    if check_username(nom_utilisateur, noms_utilisateurs):
+        print(f"Bienvenue, {nom_utilisateur} ! Vous êtes un utilisateur enregistré.")
+    else:
+        print(f"Désolé, {nom_utilisateur}. Vous n'êtes pas dans la liste des utilisateurs enregistrés.")
